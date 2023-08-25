a195f88dffb00c75e4de9f7f3869303a207d30cc
EhsanTgv <EhsanTgv@gmail.com>
2023-08-25 10:31:18 +0300

    make top navigation responsive

--- lib/layout.dart
+++ lib/layout.dart
@@ -2,17 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:web_dashboard_practice/helpers/responsiveness.dart';
 import 'package:web_dashboard_practice/widgets/large_screen.dart';
 import 'package:web_dashboard_practice/widgets/small_screen.dart';
+import 'package:web_dashboard_practice/widgets/top_nav.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({super.key});
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-      ),
+      appBar: topNavigationBar(context, scaffoldKey),
       body: const ResponsiveWidget(
         largeScreen: LargeScreen(),
         smallScreen: SmallScreen(),
---
+++ lib/widgets/top_nav.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:web_dashboard_practice/helpers/responsiveness.dart';
+
+AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
+    AppBar(
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 14),
+                  child: Image.asset(
+                    "assets/icons/logo.png",
+                    color: Colors.orange,
+                    width: 28,
+                  ),
+                )
+              ],
+            )
+          : IconButton(
+              onPressed: () {
+                key.currentState?.openDrawer();
+              },
+              icon: const Icon(Icons.menu)),
+      elevation: 0,
+    );
